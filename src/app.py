b73c76efd54e1441129baab00acd770de8a6fadc
Sven Haardiek <sven@haardiek.de>
2024-02-12 12:52:05 +0100

    move to clemens engine and remove some bbc branding

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 #
-# Web based GUI for BBC chess engine
+# Web based GUI for Clemens chess engine
 #
 
 # packages
@@ -27,9 +27,9 @@ def probe_book(pgn):
         # read book games
         book = f.read()
 
-        # init board        
+        # init board
         board = chess.Board()
-        
+
         # define response moves
         response_moves = []
 
@@ -37,28 +37,28 @@ def probe_book(pgn):
         for line in book.split('\n')[0:-1]:
             # define variation
             variation = []
-            
+
             # loop over line moves
             for move in line.split():
                 variation.append(chess.Move.from_uci(move))
-            
+
             # parse variation to SAN
             san = board.variation_san(variation)
-            
+
             # match book line line
             if pgn in san:
                 try:
                     # black move
                     if san.split(pgn)[-1].split()[0][0].isdigit():
                         response_moves.append(san.split(pgn)[-1].split()[1])
-                    
+
                     # white move
                     else:
                         response_moves.append(san.split(pgn)[-1].split()[0])
-                
+
                 except:
                     pass
-            
+
             # engine makes first move
             if pgn == '':
                 response_moves.append(san.split('1. ')[-1].split()[0])
@@ -67,21 +67,21 @@ def probe_book(pgn):
         if len(response_moves):
             print('BOOK MOVE:', random.choice(response_moves))
             return random.choice(response_moves)
-        
+
         else:
             return 0
 
 # root(index) route
 @app.route('/')
 def root():
-    return render_template('bbc.html')
+    return render_template('clemens.html')
 
 # make move API
 @app.route('/make_move', methods=['POST'])
 def make_move():
     # extract FEN string from HTTP POST request body
     pgn = request.form.get('pgn')
-    
+
     # probe opening book
     if probe_book(pgn):
         return {
@@ -90,25 +90,25 @@ def make_move():
         }
 
     # read game moves from PGN
-    game = chess.pgn.read_game(io.StringIO(pgn))    
-    
+    game = chess.pgn.read_game(io.StringIO(pgn))
+
     # init board
     board = game.board()
-    
+
     # loop over moves in game
     for move in game.mainline_moves():
         # make move on chess board
         board.push(move)
-    
+
     # create chess engine instance
-    engine = chess.engine.SimpleEngine.popen_uci('./engine/bbc_1.4')
-    
+    engine = chess.engine.SimpleEngine.popen_uci('./engine/clemens')
+
     # extract fixed depth value
     fixed_depth = request.form.get('fixed_depth')
 
     # extract move time value
     move_time = request.form.get('move_time')
-    
+
     # if move time is available
     if move_time != '0':
         if move_time == 'instant':
@@ -131,33 +131,33 @@ def make_move():
             info = engine.analyse(board, chess.engine.Limit(depth=int(fixed_depth)))
         except:
             info = {}
-    
+
     # terminate engine process
     engine.quit()
-    
+
     try:
         # extract best move from PV
         best_move = info['pv'][0]
 
         # update internal python chess board state
         board.push(best_move)
-        
-       
-        
+
+
+
         # get best score
         try:
             score = -int(str(info['score'])) / 100
-        
+
         except:
             score = str(info['score'])
-            
+
             # inverse score
             if '+' in score:
                 score = score.replace('+', '-')
-            
+
             elif '-' in score:
                 score = score.replace('-', '+')
-          
+
         return {
             'fen': board.fen(),
             'best_move': str(best_move),
@@ -167,57 +167,12 @@ def make_move():
             'nodes': info['nodes'],
             'time': info['time']
         }
-    
+
     except:
         return {
             'fen': board.fen(),
             'score': '#+1'
         }
-
-@app.route('/analytics')
-def analytics():
-    return render_template('stats.html')
-
-@app.route('/analytics/api/post', methods=['POST'])
-def post():
-    response = Response('')
-    response.headers['Access-Control-Allow-Origin'] = '*'
-
-    stats = {
-        'Date': request.form.get('date'),
-        'Url': request.form.get('url'),
-        'Agent':request.headers.get('User-Agent')
-    }
-
-    if request.headers.getlist("X-Forwarded-For"):
-       stats['Ip'] = request.headers.getlist("X-Forwarded-For")[0]
-    else:
-       stats['Ip'] = request.remote_addr
-    
-    if request.headers.get('Origin'):
-        stats['Origin'] = request.headers.get('Origin')
-    else:
-        stats['Origin'] = 'N/A'
-    
-    if request.headers.get('Referer'):
-        stats['Referer'] = request.headers.get('Referer')
-    else:
-        stats['Referer'] = 'N/A'
-    
-    with open('stats.json', 'a') as f: f.write(json.dumps(stats, indent=2) + '\n\n')
-    return response
-
-
-@app.route('/analytics/api/get')
-def get():
-    stats = []
-    
-    with open('stats.json') as f:
-        for entry in f.read().split('\n\n'):
-            try: stats.append(json.loads(entry))
-            except: pass
-              
-    return jsonify({'data': stats})
 
 # main driver
 if __name__ == '__main__':
--- src/book.py
+++ src/book.py
@@ -8,20 +8,20 @@ with open('./engine/book.txt') as f:
     # define book variations
     book_variations = []
 
-    # init board        
+    # init board
     board = chess.Board()
 
     # loop over book lines
     for line in book_raw.split('\n')[0:-1]:
         # define variation
         variation = []
-        
+
         # loop over line moves
         for move in line.split():
             variation.append(chess.Move.from_uci(move))
-        
+
         # append variation
         book_variations.append(board.variation_san(variation))
 
     print(book_variations[0])
-            
+
